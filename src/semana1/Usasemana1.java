6d5d6138f37f00a742c62c2cb94ef2a5d4939cdc
Brian <Brian@BRYAN.mshome.net>
2019-12-16 07:55:58 -0500

    Es la primera practica

---
+++ src/semana1/Usasemana1.java
@@ -0,0 +1,13 @@
+
+package semana1;
+
+
+public class Usasemana1 {
+    public static void main(String[] args) {
+        Usasemana1 s=new Usasemana1() ;
+        s.setVisible(true);
+        s.setLocaltionRelativeTo(null);
+        
+        
+    }
+}
